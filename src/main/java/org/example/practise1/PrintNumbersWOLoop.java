5c9eb43ea8a9c7e4dc945a915366dd9178fdbe3d
naveen prasanth <naveenprasanth2@gmail.com>
2024-02-05 10:28:44 +0530

    complete recurssion

--- src/main/java/org/example/practise1/PrintNumbersWOLoop.java
+++ src/main/java/org/example/practise1/PrintNumbersWOLoop.java
@@ -1,16 +1,16 @@
 package org.example.practise1;
 
 public class PrintNumbersWOLoop {
-    static int max = 100;
     public static void main(String[] args) {
-        printWithoutLoop(1);
+        int max = 100;
+        printWithoutLoop(1, max);
 
     }
 
-    private static void printWithoutLoop(int value){
-        if(value <= max){
+    private static void printWithoutLoop(int value, int max) {
+        if (value <= max) {
             System.out.println(value);
-            printWithoutLoop(++value);
+            printWithoutLoop(++value, max);
         }
     }
 }
