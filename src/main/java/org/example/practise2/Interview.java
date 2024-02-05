9df4e33b1f7aecdf50dadab112fba697432bdb78
naveen prasanth <naveenprasanth2@gmail.com>
2024-02-05 19:54:27 +0530

    string template implementation

---
+++ src/main/java/org/example/practise2/Interview.java
@@ -0,0 +1,9 @@
+package org.example.practise2;
+
+public class Interview {
+    public static void main(String[] args) {
+        String name = "naveen";
+
+        System.out.println(STR."this is good \{name}");
+    }
+}
