6e5d61985c03a62eba9c99aaa5918d91bd6a3a11
naveen prasanth <naveenprasanth2@gmail.com>
2024-02-10 11:55:12 +0530

    genericAndNewConcept

---
+++ src/main/java/org/example/generics/GenericClass.java
@@ -0,0 +1,6 @@
+package org.example.generics;
+
+record BaseBallPlayer(String name, String position){}
+public class GenericClass<T> {
+    private T field;
+}
---
+++ src/main/java/org/example/generics/RegularClass.java
@@ -0,0 +1,5 @@
+package org.example.generics;
+
+public class RegularClass {
+    private String field;
+}
