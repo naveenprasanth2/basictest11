35a095687b08933ad93a6c15feeb4590e048dcaf
naveen prasanth <naveenprasanth2@gmail.com>
2024-02-12 12:27:51 +0530

    thread sync

---
+++ src/main/java/org/example/threading/SyncDemo.java
@@ -0,0 +1,26 @@
+package org.example.threading;
+
+
+import java.util.stream.IntStream;
+
+class Counter {
+   volatile int count;
+
+    public synchronized void incrementCount() {
+            count++;
+    }
+}
+
+public class SyncDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter c = new Counter();
+        Thread t1 = new Thread(() -> IntStream.rangeClosed(1, 10000).forEach(_ -> c.incrementCount()));
+        Thread t2 = new Thread(() -> IntStream.rangeClosed(1, 10000).forEach(_ -> c.incrementCount()));
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(STR."count value is \{c.count}");
+    }
+}
