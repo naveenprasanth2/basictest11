07f24f0f0f8c8a602f7478169dc838a0073122f6
naveen prasanth <naveenprasanth2@gmail.com>
2024-02-16 19:00:56 +0530

    addRange

--- src/main/java/org/example/challanges/SumOfIntervals.java
+++ src/main/java/org/example/challanges/SumOfIntervals.java
@@ -1,10 +1,18 @@
 package org.example.challanges;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class SumOfIntervals {
     public static void main(String[] args) {
        long sum = IntStream.rangeClosed(15, 20).sum();
         System.out.println(sum);
+
+      long sum1 =  IntStream.rangeClosed(28, 31)
+                .map(x -> Stream.of(String.valueOf(x).split(""))
+                        .mapToInt(Integer::parseInt).sum()).sum();
+
+        System.out.println(sum1);
     }
 }
