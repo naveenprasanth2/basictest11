8b99b6aa74887b4bc02443fbb77972d8944af208
naveen prasanth <naveenprasanth2@gmail.com>
2024-02-19 19:13:11 +0530

    threading concepst

--- src/main/java/org/example/java8/multithreading/MyNumberPrinter.java
+++ src/main/java/org/example/java8/multithreading/MyNumberPrinter.java
@@ -1,21 +1,36 @@
 package org.example.java8.multithreading;
 
-public class MyNumberPrinter implements Runnable{
+public class MyNumberPrinter {
     int number = 1;
     private final Object object = new Object();
 
-    private void printNumbers(){
-        synchronized (object){
-            if(number <= 10){
-                if(number%2 == 0)
+    private void printNumbers() {
+        synchronized (object) {
+            try {
+                while (number <= 10) {
                     System.out.println(STR."The thread \{Thread.currentThread().getName()} printed is \{number}");
-                else System.out.println(STR."The thread \{Thread.currentThread().getName()} printed is \{number}");
+                    number++;
+                    object.notify();
+                    if (number <= 10) {
+                        object.wait();
+                    }
+                }
+            } catch (InterruptedException interruptedException) {
+                Thread.currentThread().interrupt();
+                System.out.println("Thread Interrupted");
             }
         }
     }
 
-    @Override
-    public void run() {
+    public static void main(String[] args) {
+        MyNumberPrinter printer = new MyNumberPrinter();
 
+        Runnable printTask = printer::printNumbers;
+
+        Thread thread1 = new Thread(printTask, "Thread 1");
+        Thread thread2 = new Thread(printTask, "Thread 2");
+
+        thread1.start();
+        thread2.start();
     }
 }
